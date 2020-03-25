688a4474948c12d1eb3ffacd628873e740ce1666
Natalia Banasiewicz <natalia.banasiewicz@wp.pl>
2020-03-25 12:34:19 +0100

    Add setup for sending orders to json-server fake API

--- src/components/features/OrderForm/OrderForm.js
+++ src/components/features/OrderForm/OrderForm.js
@@ -5,6 +5,37 @@ import PropTypes from 'prop-types';
 import PricingOptions from '../../../data/pricing';
 import OrderOption from '../OrderOption/OrderOption';
 import styles from './OrderForm.scss';
+import Button from '../../common/Button/Button';
+import {formatPrice} from '../../../utils/formatPrice';
+import {calculateTotal} from '../../../utils/calculateTotal';
+import settings from '../../../data/settings';
+
+const sendOrder = (options, tripCost) => {
+  const totalCost = formatPrice(calculateTotal(tripCost, options));
+
+  const payload = {
+    ...options,
+    totalCost,
+  };
+
+  const url = settings.db.url + '/' + settings.db.endpoint.orders;
+
+  const fetchOptions = {
+    cache: 'no-cache',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  };
+
+  fetch(url, fetchOptions)
+    .then(function(response){
+      return response.json();
+    }).then(function(parsedResponse){
+      console.log('parsedResponse', parsedResponse);
+    });
+};
 
 const OrderForm = ({tripCost, options, setOrderOption}) => (  
   <Row center="md">
@@ -23,6 +54,9 @@ const OrderForm = ({tripCost, options, setOrderOption}) => (
     <Col xs={12}>
       <OrderSummary tripCost={tripCost} tripOptions={options}/>
     </Col>
+
+    <Button onClick={() => sendOrder(options, tripCost)}>Order now!</Button>
+
   </Row>
 );
 
